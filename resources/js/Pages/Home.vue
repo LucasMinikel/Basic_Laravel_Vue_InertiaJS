78a77b460b6fe998bc23c73cc125c4dad73199dd
Lucas Minikel <lucasminikel.m@gmail.com>
2022-02-03 20:24:26 -0300

    Passando dados para Vue View

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <p>Usando</p>
+        <ul>
+            <li v-for="framework of frameworks" v-text="framework"></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    },
+};
+</script>
